a7297b6510ba6cd8659938a9fef3596ab1745553
dingxin <wdx7266@vip.qq.com>
2018-08-30 21:14:56 +0800

    update

--- smart_contract/asset_allocation.py
+++ smart_contract/asset_allocation.py
@@ -1,4 +1,6 @@
+from boa.interop.Neo.Action import RegisterAction
 from boa.interop.System.ExecutionEngine import *
+from boa.interop.Ontology.Native import *
 from boa.interop.Neo.Blockchain import *
 from boa.interop.Neo.Storage import *
 from boa.interop.Neo.Runtime import *
@@ -6,6 +8,9 @@ from boa.interop.Neo.Block import *
 from boa.builtins import *
 
 ctx = GetContext()
+contract_address = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01')
+
+OnTransfer = RegisterAction('transfer', 'address_from', 'address_to', 'amount')
 
 
 def create_template(rule):
@@ -20,13 +25,13 @@ def create_template(rule):
         template_id = sha256(str_value)
         Put(ctx, template_id, rule)
         Log(template_id)
-        Put(ctx, template_id, 0)
+        Put(ctx, concat(template_id, 'count'), 0)
         return True
     except Exception:
         return False
 
 
-def create_instance(template_id, metadata, payments, payer_threshold, payees, reviewer=''):
+def create_instance(template_id, metadata, payments, payer_threshold, payees, reviewer):
     """
     :param template_id: int
     :param metadata: str
@@ -40,15 +45,17 @@ def create_instance(template_id, metadata, payments, payer_threshold, payees, re
         current_height = GetHeight()
         current_block = GetBlock(current_height)
         current_block_timestamp = current_block.Timestamp
-        count = Get(ctx, template_id)
+        count = Get(ctx, concat(template_id, 'count'))
         data = concat(template_id, current_block_timestamp)
-        data = concat(data, count)
+        data = concat(data, str(count))
         instance_id = sha256(data)
         Put(ctx, concat(instance_id, 'metadata'), metadata)
         Put(ctx, concat(instance_id, 'payments'), payments)
         Put(ctx, concat(instance_id, 'threshold'), payer_threshold)
         Put(ctx, concat(instance_id, 'payees'), payees)
         Put(ctx, concat(instance_id, 'reviewer'), reviewer)
+        Put(ctx, concat(instance_id, 'balance'), 0)
+        Put(ctx, concat(instance_id, 'lock'), False)
         Notify(['create', instance_id])
         count += 1
         Put(ctx, template_id, count)
@@ -64,8 +71,16 @@ def input_asset(instance_id, amount, payer):
     :param amount: int
     :param payer: str
     """
-    Put(ctx, concat(instance_id, 'lock'), False)
-    Notify(['input', instance_id, amount, payer])
+    script_hash = GetExecutingScriptHash()
+    is_lock = Get(ctx, concat(instance_id, 'lock'))
+    if is_lock:
+        return False
+    else:
+        OnTransfer(payer, script_hash, amount)
+        balance = Get(ctx, concat(instance_id, 'balance'))
+        balance += amount
+        Put(ctx, concat(instance_id, 'balance'), balance)
+        Notify(['input', instance_id, amount, payer])
 
 
 def lock(instance_id, lock_time, payers):
@@ -75,8 +90,8 @@ def lock(instance_id, lock_time, payers):
     :param lock_time: str
     :param payers: list
     """
-    state = Get(ctx, concat(instance_id, 'lock'))
-    if state:
+    is_lock = Get(ctx, concat(instance_id, 'lock'))
+    if is_lock:
         return False
     else:
         Put(ctx, concat(instance_id, 'lock'), True)
@@ -99,6 +114,16 @@ def confirm(instance_id, confirmer):
     if current_block_timestamp < lock_time:
         return False
     else:
+        from_acct = GetExecutingScriptHash()
+        payees = Get(ctx, concat(instance_id, 'payees'))
+        balance = Get(ctx, concat(instance_id, 'balance'))
+        quota = Get(ctx, concat(instance_id, 'reviewerQuota'))
+        if len(quota) == 0:
+            quota = Get(ctx, instance_id)
+        for index in range(0, len(payees)):
+            amount = balance * quota[index]
+            param = state(from_acct, payees[index], amount)
+            Invoke(amount, contract_address, 'transfer', [param])
         Put(ctx, concat(instance_id, 'lock'), False)
         Notify(["Confirm", instance_id, confirmer])
         return True
@@ -113,9 +138,13 @@ def set_quota(instance_id, quota, reviewer):
     :return:
     """
     if CheckWitness(reviewer):
-        Put(ctx, concat(instance_id, 'reviewerQuota'), quota)
-        Notify(['Quota', instance_id, quota])
-        return True
+        payees = Get(ctx, concat(instance_id, 'payees'))
+        if len(payees) > len(quota):
+            return False
+        else:
+            Put(ctx, concat(instance_id, 'reviewerQuota'), quota)
+            Notify(['Quota', instance_id, quota])
+            return True
     else:
         return False
 
@@ -141,17 +170,17 @@ def main(operation, args):
     :param args: a list of arguments ( which may be empty, but not absent )
     :return: indicating the successful execution of the smart contract
     """
-    if operation == 'CreateTemplate':
+    if operation == 'create_template':
         create_template(args[0])
         return True
-    elif operation == 'CreateInstance':
+    elif operation == 'create_instance':
         if len(args) == 4:
-            return create_instance(args[0], args[1], args[2], args[3], args[4])
+            return create_instance(args[0], args[1], args[2], args[3], args[4], '')
         elif len(args) == 5:
             return create_instance(args[0], args[1], args[2], args[3], args[4], args[5])
         else:
             return False
-    elif operation == 'InputAsset':
+    elif operation == 'input_asset':
         if len(args) == 3:
             payer = GetCallingScriptHash()
             if payer != args[2]:
@@ -159,9 +188,22 @@ def main(operation, args):
             return input_asset(args[0], args[1], args[2])
         else:
             return False
-    elif operation == 'Confirm':
+    elif operation == 'lock':
+        if len(args) == 3:
+            return
+    elif operation == 'confirm':
         if len(args) == 2:
             return confirm(args[0], args[1])
+        else:
+            return False
+    elif operation == 'set_quota':
+        if len(args) == 3:
+            return set_quota(args[0], args[1], args[2])
+        else:
+            return False
+    elif operation == 'refund':
+        if len(args) == 2:
+            return refund(args[0], args[1])
         else:
             return False
     else:
